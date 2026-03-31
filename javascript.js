6e73771470c41774e13ba036208505cbb3661905
“pminh530” <250648659+pminh530-maker@users.noreply.github.com>
2026-03-31 11:53:43 +0700

    Create a hover effect

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-// Main task: Create a 16 x 16 grid using for loops.
+// Task 1: Create a 16 x 16 grid using for loops.
 
 let container = document.querySelector(".container");
 
@@ -8,5 +8,9 @@ for (let i = 0; i < size * size ; i++) {
         let square = document.createElement("div");
         square.classList.add("square");
         container.appendChild(square);
-        console.log(square.childElementCount);
+        // Task 2: Create a hover effect with addEventListner()
+        square.addEventListener("mouseenter", (e) => {
+                square.style.backgroundColor = "violet";
+        })
 }
+        
